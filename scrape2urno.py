1a387d430ea04dbe5e8fa8e23c61ad5d99898eba
fakebenjay <fakebenjay@gmail.com>
2022-11-03 04:13:52 -0400

    goodnight

--- scrape2urno.py
+++ scrape2urno.py
@@ -12,86 +12,79 @@ import pandas as pd
 import numpy as np
 from termcolor import colored
 
-def append_blank(df, cidade, zona_num, secao_num):
-    pd.concat([df, pd.Series({
-        'cidade':cidade,
-        'municipo':'',
-        'zona':zona_num,
-        'secao':secao_num,
-        'polling_place':'',
-        'lula_voto':'',
-        'lula_pct':'',
-        'bolsonaro_voto':'',
-        'bolsonaro_pct':'',
-        'eligible_voters':'',
-        'attending_voters':'',
-        'noshow_voters':'',
-        'turnout_pct':'',
-        'valid_votes':'',
-        'valid_pct':'',
-        'votos_legenda':'',
-        'blank_votes':'',
-        'null_votes':'',
-        'votes_counted':'',
-        'machine_type':''
-    })], ignore_index = True)
-
-df = pd.read_excel('cidade_dir.xlsx')
+# df = pd.read_excel('cidade_dir.xlsx')
 
 index = 0
+counter = 1
 secao_index = 1
+secaos_length = 1
 val="https://resultados.tse.jus.br/oficial/app/index.html#/eleicao;uf=zz;ufbu=zz/dados-de-urna;e=e545/boletim-de-urna"
 df = pd.DataFrame(columns=['cidade', 'municipo', 'zona', 'secao', 'polling_place','lula_voto', 'lula_pct', 'bolsonaro_voto', 'bolsonaro_pct', 'eligible_voters', 'attending_voters','noshow_voters','turnout_pct','valid_votes','valid_pct','votos_legenda','blank_votes','null_votes','votes_counted','machine_type'])
 
+lula_voto = 0
+bolsonaro_voto = 0
+lula_pct = 0
+bolsonaro_pct = 0
+
+
 while index < 181:
     try:
-        driver = webdriver.Chrome()
-        driver.get(val)
-
-        modal_select = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, 'app-selecionar-localidade2'))
-        )
-
-        modal_select.click()
-
-        cidades_list = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, "#cdk-overlay-0"))
-        )
-
-        cidades = driver.find_elements(By.CSS_SELECTOR, "#cdk-overlay-0 .mat-option")
-
-        cidade = cidades[index].text
-        cidades[index].click()
-
-        confirmar = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, '.p-5 ion-button'))
-        )
-        confirmar.click()
-
-        zona = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, 'mat-form-field.ng-tns-c73-0'))
-        )
-
-        zona.click()
-        zonas = driver.find_elements(By.CSS_SELECTOR, ".mat-select-panel mat-option")
-        zonas[1].click()
-
-        secao = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, 'mat-form-field:nth-child(2)'))
-        )
-
-        secao.click()
-        secaos = driver.find_elements(By.CSS_SELECTOR, ".mat-select-panel mat-option")
-        secaos_length = len(secaos) - 1
-
         if secao_index <= secaos_length:
+            driver = webdriver.Chrome()
+            driver.get(val)
+
+            modal_select = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, 'app-selecionar-localidade2'))
+            )
+
+            modal_select.click()
+
+            cidades_autocomplete = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, '.mt-12 .local-campo input.mat-autocomplete-trigger'))
+            )
+
+            cidades_autocomplete.click()
+
+            cidades_list = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, "#cdk-overlay-0"))
+            )
+
+            cidades_test = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, "#cdk-overlay-0 .mat-option"))
+            )
+
+            cidades = driver.find_elements(By.CSS_SELECTOR, "#cdk-overlay-0 .mat-option")
+
+            cidade = cidades[index].text
+            cidades[index].click()
+
+            confirmar = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, '.p-5 ion-button'))
+            )
+            confirmar.click()
+
+            zona = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, 'mat-form-field.ng-tns-c73-0'))
+            )
+
+            zona.click()
+            zonas = driver.find_elements(By.CSS_SELECTOR, ".mat-select-panel mat-option")
+            zonas[1].click()
+
+            secao = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, 'mat-form-field:nth-child(2)'))
+            )
+
+            secao.click()
+            secaos = driver.find_elements(By.CSS_SELECTOR, ".mat-select-panel mat-option")
+            secaos_length = len(secaos) - 1
+
             secaos[secao_index].click()
 
             pesquisar = WebDriverWait(driver, 10).until(
                 EC.element_to_be_clickable((By.CSS_SELECTOR, 'button.bg-yellow-500'))
             )
             pesquisar.click()
-
 
             if secao_index == 1:
                 rdv = WebDriverWait(driver, 10).until(
@@ -103,12 +96,18 @@ while index < 181:
                     EC.presence_of_element_located((By.CSS_SELECTOR, "ng-component")) #This is a dummy element
                 )
 
-                if "Não foi possível completar a operação" in ng_component.text:
-                    df = append_blank(df, cidade, zona_num, secao_num)
-                    index += 1
-                    secao_index = 1
-                    driver.quit()
-                else:
+                if "Não foi possível completar a operação" not in ng_component.text:
+                #     index += 1
+                #     secao_index = 1
+                #     secaos_length = 1
+                #
+                #     lula_voto = 0
+                #     bolsonaro_voto = 0
+                #     lula_pct = 0
+                #     bolsonaro_pct = 0
+                #
+                #     driver.quit()
+                # else:
                     boletim_de_urna = WebDriverWait(driver, 10).until(
                         EC.element_to_be_clickable((By.CSS_SELECTOR, "li.mr-5:nth-child(1) a")) #This is a dummy element
                     )
@@ -122,15 +121,11 @@ while index < 181:
                 EC.presence_of_element_located((By.CSS_SELECTOR, "app-boletim-de-urna")) #This is a dummy element
             )
 
-
-
             soup = BeautifulSoup(elem.get_attribute("innerHTML"), features="lxml")
             cidade = soup.find("div", class_="leading-tight").text.replace(', ZZ', '').strip()
             zona_secao = soup.find_all("div", class_="mat-select-value")
             zona_num = zona_secao[0].text.split(' ')[1].strip()
             secao_num = zona_secao[1].text.split(' ')[1].strip()
-
-            print(cidade + ': Zona ' + zona_num + ', Seção ' + secao_num + ' - ' + str(secao_index) + " do " + str(secaos_length))
 
             if "Situação da Seção:\nTotalizada" in grid_cols.text:
                 urno_stats = soup.find_all("div", class_="mb-4")[1].find_all("div", class_="mb-2")
@@ -194,21 +189,68 @@ while index < 181:
                     'null_votes':int(nulos),
                     'votes_counted':int(apurado),
                     'machine_type':urno_type
-                })], ignore_index = True)
+                }).to_frame().T], ignore_index = True)
 
             else:
-                df = append_blank(df, cidade, zona_num, secao_num)
+                lula_voto = 0
+                bolsonaro_voto = 0
+                lula_pct = 0
+                bolsonaro_pct = 0
 
-
+                df = pd.concat([df, pd.Series({
+                    'cidade':cidade,
+                    'municipo':'',
+                    'zona':zona_num,
+                    'secao':secao_num,
+                    'polling_place':'',
+                    'lula_voto':'',
+                    'lula_pct':'',
+                    'bolsonaro_voto':'',
+                    'bolsonaro_pct':'',
+                    'eligible_voters':'',
+                    'attending_voters':'',
+                    'noshow_voters':'',
+                    'turnout_pct':'',
+                    'valid_votes':'',
+                    'valid_pct':'',
+                    'votos_legenda':'',
+                    'blank_votes':'',
+                    'null_votes':'',
+                    'votes_counted':'',
+                    'machine_type':''
+                }).to_frame().T], ignore_index = True)
         if secao_index <= secaos_length:
+            if lula_voto == 0:
+                endbit = ' - NADA'
+            else:
+                if lula_pct > bolsonaro_pct:
+                    resultado = colored(' Lula! ', 'white', 'on_red')
+                    score = lula_pct
+                    votos = lula_voto
+                elif lula_pct < bolsonaro_pct:
+                    resultado = resultado = colored(" Fuckão :( ", 'white', 'on_blue')
+                    score = bolsonaro_pct
+                    votos = bolsonaro_voto
+                else:
+                    resultado = "Tie"
+                    score = lula_pct
+                    votos = lula_voto
+                endbit = ' - ' + resultado + ' (' + str(round(score*100,2)) + '%, ' + str(votos) + ' votos)'
+
+            print(str(counter) + ". " + cidade + ' (' + str(index + 1) + '): Zona ' + zona_num + ', Seção ' + secao_num + ' - ' + str(secao_index) + " do " + str(secaos_length) + endbit)
+
             secao_index += 1
+
+            counter += 1
+            with open('data-2o-urno.csv', 'w') as csv_file:
+                df.to_csv(index=False, path_or_buf=csv_file, encoding='utf-8')
         else:
             index += 1
             secao_index = 1
+            secaos_length = 1
             driver.quit()
+    except Exception as e:
 
+        print('error -- counter ' + str(counter) + ', index ' + str(index) + ', secao_index ' + str(secao_index) + ' -- ' + cidade)
     finally:
         driver.quit()
-
-with open('data-2o-urno.csv', 'w') as csv_file:
-    df.to_csv(index=False, path_or_buf=csv_file)
