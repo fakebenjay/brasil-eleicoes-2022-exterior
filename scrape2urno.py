420bfacdb31e7933af6c0eff312aaa9289dcbc3f
fakebenjay <fakebenjay@gmail.com>
2022-11-04 10:19:13 -0400

    got it

--- scrape2urno.py
+++ scrape2urno.py
@@ -17,9 +17,10 @@ from termcolor import colored
 index = 0
 counter = 1
 secao_index = 1
-secaos_length = 1
+secoes_length = 1
 val="https://resultados.tse.jus.br/oficial/app/index.html#/eleicao;uf=zz;ufbu=zz/dados-de-urna;e=e545/boletim-de-urna"
-df = pd.DataFrame(columns=['cidade', 'municipo', 'zona', 'secao', 'polling_place','lula_voto', 'lula_pct', 'bolsonaro_voto', 'bolsonaro_pct', 'eligible_voters', 'attending_voters','noshow_voters','turnout_pct','valid_votes','valid_pct','votos_legenda','blank_votes','null_votes','votes_counted','machine_type'])
+df = pd.DataFrame(columns=['cidade', 'municipo', 'zona', 'secao', 'polling_place','lula_voto', 'lula_pct', 'bolsonaro_voto', 'bolsonaro_pct', 'eligible_voters', 'attending_voters','noshow_voters','turnout_pct','valid_votes','valid_pct','votos_legenda','blank_votes','null_votes','votes_counted','machine_type','flag', 'dia', 'country_pt', 'country_en', 'venue', 'local_pt', 'local_en', 'city_pt', 'city_en'])
+secoes_df = pd.read_excel('venues.xlsx', dtype='str')
 
 lula_voto = 0
 bolsonaro_voto = 0
@@ -29,7 +30,7 @@ bolsonaro_pct = 0
 
 while index < 181:
     try:
-        if secao_index <= secaos_length:
+        if secao_index <= secoes_length:
             driver = webdriver.Chrome()
             driver.get(val)
 
@@ -76,42 +77,41 @@ while index < 181:
             )
 
             secao.click()
-            secaos = driver.find_elements(By.CSS_SELECTOR, ".mat-select-panel mat-option")
-            secaos_length = len(secaos) - 1
+            secoes = driver.find_elements(By.CSS_SELECTOR, ".mat-select-panel mat-option")
+            secoes_length = len(secoes) - 1
 
-            secaos[secao_index].click()
+            secoes[secao_index].click()
 
             pesquisar = WebDriverWait(driver, 10).until(
                 EC.element_to_be_clickable((By.CSS_SELECTOR, 'button.bg-yellow-500'))
             )
             pesquisar.click()
 
-            if secao_index == 1:
-                rdv = WebDriverWait(driver, 10).until(
-                    EC.element_to_be_clickable((By.CSS_SELECTOR, "li.mr-5:nth-child(2) a")) #This is a dummy element
-                )
-                rdv.click()
+            rdv = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, "li.mr-5:nth-child(2) a")) #This is a dummy element
+            )
+            rdv.click()
 
-                ng_component = WebDriverWait(driver, 10).until(
-                    EC.presence_of_element_located((By.CSS_SELECTOR, "ng-component")) #This is a dummy element
-                )
+            ng_component = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, "ng-component")) #This is a dummy element
+            )
 
-                if "Não foi possível completar a operação" not in ng_component.text:
-                #     index += 1
-                #     secao_index = 1
-                #     secaos_length = 1
-                #
-                #     lula_voto = 0
-                #     bolsonaro_voto = 0
-                #     lula_pct = 0
-                #     bolsonaro_pct = 0
-                #
-                #     driver.quit()
-                # else:
-                    boletim_de_urna = WebDriverWait(driver, 10).until(
-                        EC.element_to_be_clickable((By.CSS_SELECTOR, "li.mr-5:nth-child(1) a")) #This is a dummy element
-                    )
-                    boletim_de_urna.click()
+            if "Não foi possível completar a operação" not in ng_component.text:
+            #     index += 1
+            #     secao_index = 1
+            #     secoes_length = 1
+            #
+            #     lula_voto = 0
+            #     bolsonaro_voto = 0
+            #     lula_pct = 0
+            #     bolsonaro_pct = 0
+            #
+            #     driver.quit()
+            # else:
+                boletim_de_urna = WebDriverWait(driver, 10).until(
+                    EC.element_to_be_clickable((By.CSS_SELECTOR, "li.mr-5:nth-child(1) a")) #This is a dummy element
+                )
+                boletim_de_urna.click()
 
             grid_cols = WebDriverWait(driver, 10).until(
                 EC.presence_of_element_located((By.CSS_SELECTOR, ".grid-cols-3"))
@@ -126,6 +126,16 @@ while index < 181:
             zona_secao = soup.find_all("div", class_="mat-select-value")
             zona_num = zona_secao[0].text.split(' ')[1].strip()
             secao_num = zona_secao[1].text.split(' ')[1].strip()
+
+            flag = secoes_df.loc[secoes_df["secao"] == secao_num, "flag"].values[0]
+            dia = secoes_df.loc[secoes_df["secao"] == secao_num, "dia"].values[0]
+            country_pt = secoes_df.loc[secoes_df["secao"] == secao_num, "country_pt"].values[0]
+            country_en = secoes_df.loc[secoes_df["secao"] == secao_num, "country_en"].values[0]
+            venue = secoes_df.loc[secoes_df["secao"] == secao_num, "venue"].values[0]
+            local_pt = secoes_df.loc[secoes_df["secao"] == secao_num, "local_pt"].values[0]
+            local_en = secoes_df.loc[secoes_df["secao"] == secao_num, "local_en"].values[0]
+            city_pt = secoes_df.loc[secoes_df["secao"] == secao_num, "city_pt"].values[0]
+            city_en = secoes_df.loc[secoes_df["secao"] == secao_num, "city_en"].values[0]
 
             if "Situação da Seção:\nTotalizada" in grid_cols.text:
                 urno_stats = soup.find_all("div", class_="mb-4")[1].find_all("div", class_="mb-2")
@@ -165,7 +175,6 @@ while index < 181:
                 nulos = bottom_stats[5].find("p", class_="font-bold").text.strip()
                 apurado = bottom_stats[6].find("p", class_="font-bold").text.strip()
 
-
                 lula_pct = int(lula_voto) / int(votos_nominais)
                 bolsonaro_pct = int(bolsonaro_voto) / int(votos_nominais)
                 df = pd.concat([df, pd.Series({
@@ -188,7 +197,16 @@ while index < 181:
                     'blank_votes':int(branco),
                     'null_votes':int(nulos),
                     'votes_counted':int(apurado),
-                    'machine_type':urno_type
+                    'machine_type':urno_type,
+                    'flag':flag,
+                    'dia':dia,
+                    'country_pt':country_pt,
+                    'country_en':country_en,
+                    'venue':venue,
+                    'local_pt':local_pt,
+                    'local_en':local_en,
+                    'city_pt':city_pt,
+                    'city_en':city_en
                 }).to_frame().T], ignore_index = True)
 
             else:
@@ -217,11 +235,20 @@ while index < 181:
                     'blank_votes':'',
                     'null_votes':'',
                     'votes_counted':'',
-                    'machine_type':''
+                    'machine_type':'',
+                    'flag':flag,
+                    'dia':dia,
+                    'country_pt':country_pt,
+                    'country_en':country_en,
+                    'venue':venue,
+                    'local_pt':local_pt,
+                    'local_en':local_en,
+                    'city_pt':city_pt,
+                    'city_en':city_en
                 }).to_frame().T], ignore_index = True)
-        if secao_index <= secaos_length:
+        if secao_index <= secoes_length:
             if lula_voto == 0:
-                endbit = ' - NADA'
+                endbit = ' - NADA ❌'
             else:
                 if lula_pct > bolsonaro_pct:
                     resultado = colored(' Lula! ', 'white', 'on_red')
@@ -232,12 +259,21 @@ while index < 181:
                     score = bolsonaro_pct
                     votos = bolsonaro_voto
                 else:
-                    resultado = "Tie"
+                    resultado = colored("Tie", 'black', 'on_white')
                     score = lula_pct
                     votos = lula_voto
-                endbit = ' - ' + resultado + ' (' + str(round(score*100,2)) + '%, ' + str(votos) + ' votos)'
 
-            print(str(counter) + ". " + cidade + ' (' + str(index + 1) + '): Zona ' + zona_num + ', Seção ' + secao_num + ' - ' + str(secao_index) + " do " + str(secaos_length) + endbit)
+                if dia == 'Manual':
+                    type_emoji = '🗳️'
+                elif dia == 'Não':
+                    type_emoji = '❌'
+                else:
+                    type_emoji = '💻'
+
+
+                endbit = ' - ' + resultado + ' ' + type_emoji + '  (' + str(round(score*100,2)) + '%, ' + str(votos) + ' votos)'
+
+            print(str(counter) + ". " + local_pt + ', ' + country_pt + ' ' + flag + '   ('+ cidade +' #' + str(index + 1) + '): Seção ' + secao_num + ' - ' + str(secao_index) + " do " + str(secoes_length) + endbit)
 
             secao_index += 1
 
@@ -247,10 +283,9 @@ while index < 181:
         else:
             index += 1
             secao_index = 1
-            secaos_length = 1
+            secoes_length = 1
             driver.quit()
     except Exception as e:
-
         print('error -- counter ' + str(counter) + ', index ' + str(index) + ', secao_index ' + str(secao_index) + ' -- ' + cidade)
     finally:
         driver.quit()
